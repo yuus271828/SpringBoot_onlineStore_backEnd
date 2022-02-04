42c7d5be51a90f48e8b71d5ae74fde3b5e1c6b6f
Yu-Sheng <yuus271828@gmail.com>
2022-02-04 21:34:34 +0800

    modify ApplicationTests, add @Runtwith, @SpringBootTest

--- src/test/java/com/ohiji/OnlineStoreOhijiBackEndApplicationTests.java
+++ src/test/java/com/ohiji/OnlineStoreOhijiBackEndApplicationTests.java
@@ -1,13 +1,20 @@
 package com.ohiji;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest
-class OnlineStoreOhijiBackEndApplicationTests {
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = com.ohiji.OnlineStoreOhijiBackEndApplication.class)
+public class OnlineStoreOhijiBackEndApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void contextLoads(ApplicationContext context) {
+		assertThat(context).isNotNull();
 	}
 
 }
